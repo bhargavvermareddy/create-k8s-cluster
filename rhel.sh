560b5e56f0c80db73f548b834d4c7e140b0eb406
Bhargav Vermareddy <bhargav.vermareddy@gmail.com>
2024-08-03 14:00:55 +0530

    updated the scripts with echo logging and added calico.yaml for networking

--- rhel.sh
+++ rhel.sh
@@ -1,35 +1,52 @@
 # Run following commands in both master and worker node(s)
 # https://github.com/cri-o/packaging/blob/main/README.md#usage
 
+set +e
+
+set +x
+
 # Export environment variables for Kubernetes and cri-o runtime
 export KUBERNETES_VERSION=v1.30 && \
 export CRIO_VERSION=v1.30
 
+echo "==> Disabling Swap"
 sudo swapoff -a # Disabling swap
 sudo sed -e '/swap/s/^/#/g' -i /etc/fstab # Disabling swap permanently
 
 # setting sexliunx to Permissive mode
+echo "==> Disabling SELinux"
 sudo setenforce 0
 sudo sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
 
 
 # opening required ports for k8s
-sudo firewall-cmd --zone=public --permanent --add-port=6443/tcp && \        # Kubernetes API server
-sudo firewall-cmd --zone=public --permanent --add-port=2379-2380/tcp && \   # etcd server client API
-sudo firewall-cmd --zone=public --permanent --add-port=10250/tcp && \   	# Kubelet API
-sudo firewall-cmd --zone=public --permanent --add-port=10251/tcp && \       # kube-scheduler
-sudo firewall-cmd --zone=public --permanent --add-port=10252/tcp && \       # kube-controller-manager
-sudo firewall-cmd --zone=public --permanent --add-port=10255/tcp && \       # Read-only Kubelet API
-sudo firewall-cmd --zone=public --permanent --add-port=5473/tcp             # ClusterControlPlaneConfig API
+# 6443/tcp   # Kubernetes API server
+# 2379-2380  # etcd server client API
+# 10250/tcp  # Kubelet API
+# 10251/tcp  # kube-scheduler
+# 10252/tcp  # kube-controller-manager
+# 10255/tcp  # Read-only Kubelet API
+# 5473/tcp   # ClusterControlPlaneConfig API
+echo "==> Opening required ports for K8s cluster"
+sudo firewall-cmd --zone=public --permanent --add-port=6443/tcp && \
+sudo firewall-cmd --zone=public --permanent --add-port=2379-2380/tcp && \
+sudo firewall-cmd --zone=public --permanent --add-port=10250/tcp && \
+sudo firewall-cmd --zone=public --permanent --add-port=10251/tcp && \
+sudo firewall-cmd --zone=public --permanent --add-port=10252/tcp && \
+sudo firewall-cmd --zone=public --permanent --add-port=10255/tcp && \
+sudo firewall-cmd --zone=public --permanent --add-port=5473/tcp
 
+echo "==> Reloading firewall-cmd"
 sudo firewall-cmd --reload
 
+echo "==> Applying modprobes"
 sudo modprobe br_netfilter && \
 sudo modprobe ip_vs && \
 sudo modprobe ip_vs_rr && \
 sudo modprobe ip_vs_wrr && \
 sudo modprobe ip_vs_sh && \
 sudo modprobe overlay
+
 
 cat <<EOF | sudo tee /etc/modules-load.d/kubernetes.conf
 br_netfilter
@@ -49,6 +66,7 @@ EOF
 sudo sysctl --system
 
 # Add the Kubernetes repository
+echo "==> Adding Kubernetes repository"
 cat <<EOF | sudo tee /etc/yum.repos.d/kubernetes.repo
 [kubernetes]
 name=Kubernetes
@@ -59,6 +77,7 @@ gpgkey=https://pkgs.k8s.io/core:/stable:/$KUBERNETES_VERSION/rpm/repodata/repomd
 EOF
 
 # Add the CRI-O repository
+echo "==> Adding cri-o repository"
 cat <<EOF | sudo tee /etc/yum.repos.d/cri-o.repo
 [cri-o]
 name=CRI-O
@@ -69,11 +88,13 @@ gpgkey=https://pkgs.k8s.io/addons:/cri-o:/stable:/$CRIO_VERSION/rpm/repodata/rep
 EOF
 
 # Install the packages
+echo "==> Installing the required packages"
 sudo dnf install -y container-selinux
 sudo dnf install -y cri-o kubelet kubeadm kubectl --disableexcludes=kubernetes
 sudo dnf mark install cri-o kubelet kubeadm kubectl # use 'dnf mark remove' to remove the mark
 
 # Enabling kubelet and crio to run even after node restarts
+echo "==> Enabling and starting kubelet and cri-o services using systemctl"
 sudo systemctl enable kubelet && sudo systemctl start kubelet
 sudo systemctl enable crio && sudo systemctl start crio
 
@@ -81,8 +102,10 @@ sudo systemctl enable crio && sudo systemctl start crio
 
 # Run the following commands only in master node(s)
 
+echo "==> Pulling necessary images for starting k8s cluster"
 sudo kubeadm config images pull # optional command, because 'kubeadm init' command will pull images
 
+echo "==> Initializing the K8s cluster using kubeadm init command"
 sudo kubeadm init --pod-network-cidr=10.244.0.0/16 --apiserver-advertise-address=<master-node-ip/loadbalancer-ip>
 
 # By default, your cluster will not schedule Pods on the control plane nodes for security reasons. If you want to be able to schedule Pods on the control plane nodes, for example for a single machine Kubernetes cluster, run:
--- ubuntu.sh
+++ ubuntu.sh
@@ -1,25 +1,38 @@
 # Run following commands in both master and worker node(s)
 # https://github.com/cri-o/packaging/blob/main/README.md#usage
 
+set +e
+
+set +x
+
 # Export environment variables for Kubernetes and cri-o runtime
 export KUBERNETES_VERSION=v1.30 && \
 export CRIO_VERSION=v1.30
 
-
+echo "==> Disabling Swap"
 sudo swapoff -a # Disabling swap
 sed -e '/swap/s/^/#/g' -i /etc/fstab # Disabling swap permanently
 
 # opening required ports for k8s
-sudo ufw allow 6443/tcp && \        # Kubernetes API server
-sudo ufw allow 2379:2380/tcp && \   # etcd server client API
-sudo ufw allow 10250/tcp && \   	# Kubelet API
-sudo ufw allow 10251/tcp && \       # kube-scheduler
-sudo ufw allow 10252/tcp && \       # kube-controller-manager
-sudo ufw allow 10255/tcp && \       # Read-only Kubelet API
-sudo ufw allow 5473/tcp             # ClusterControlPlaneConfig API
+# 6443/tcp   # Kubernetes API server
+# 2379-2380  # etcd server client API
+# 10250/tcp  # Kubelet API
+# 10251/tcp  # kube-scheduler
+# 10252/tcp  # kube-controller-manager
+# 10255/tcp  # Read-only Kubelet API
+# 5473/tcp   # ClusterControlPlaneConfig API
+echo "==> Opening required ports for K8s cluster"
+sudo ufw allow 6443/tcp && \
+sudo ufw allow 2379:2380/tcp && \
+sudo ufw allow 10250/tcp && \
+sudo ufw allow 10251/tcp && \
+sudo ufw allow 10252/tcp && \
+sudo ufw allow 10255/tcp && \
+sudo ufw allow 5473/tcp
 
 # sudo iptables -L -n
 
+echo "==> Applying modprobes"
 sudo modprobe br_netfilter && \
 sudo modprobe ip_vs && \
 sudo modprobe ip_vs_rr && \
@@ -45,16 +58,19 @@ EOF
 sudo sysctl --system
 
 # Add the Kubernetes repository
+echo "==> Adding Kubernetes repository"
 curl -fsSL https://pkgs.k8s.io/core:/stable:/$KUBERNETES_VERSION/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 
 echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/$KUBERNETES_VERSION/deb/ /" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
 # Add the CRI-O repository
+echo "==> Adding cri-o repository"
 curl -fsSL https://pkgs.k8s.io/addons:/cri-o:/stable:/$CRIO_VERSION/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/cri-o-apt-keyring.gpg
 
 echo "deb [signed-by=/etc/apt/keyrings/cri-o-apt-keyring.gpg] https://pkgs.k8s.io/addons:/cri-o:/stable:/$CRIO_VERSION/deb/ /" | sudo tee /etc/apt/sources.list.d/cri-o.list
 
 # Install the packages
+echo "==> Installing the required packages"
 sudo apt update
 sudo apt install -y software-properties-common curl
 sudo apt install -y cri-o kubelet kubeadm kubectl
@@ -62,6 +78,7 @@ sudo apt-mark hold cri-o kubelet kubeadm kubectl
 sudo apt-mark showhold
 
 # Enabling kubelet and crio to run even after node restarts
+echo "==> Enabling and starting kubelet and cri-o services using systemctl"
 sudo systemctl enable kubelet && sudo systemctl start kubelet
 sudo systemctl enable crio && sudo systemctl start crio
 
@@ -69,8 +86,10 @@ sudo systemctl enable crio && sudo systemctl start crio
 
 # Create cluster with kubeadm, run following command only on your master node(s)
 
+echo "==> Pulling necessary images for starting k8s cluster"
 sudo kubeadm config images pull # optional command, because 'kubeadm init' command will pull images
 
+echo "==> Initializing the K8s cluster using kubeadm init command"
 sudo kubeadm init --pod-network-cidr=10.244.0.0/16 --apiserver-advertise-address=<master-node-ip/loadbalancer-ip>
 
 # By default, your cluster will not schedule Pods on the control plane nodes for security reasons. If you want to be able to schedule Pods on the control plane nodes, for example for a single machine Kubernetes cluster, run:
